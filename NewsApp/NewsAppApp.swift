a89ffef3ac901c6d89968f12656067fd35af4256
Lluis Armengol <armengolluis@gmail.com>
2022-02-23 23:09:16 +0100

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Lluis Armengol on 23/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
